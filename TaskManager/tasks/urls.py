0223013ac9dc965134ddb609959637ed4dd324a4
Tasnim1357 <bellhossain12@gmail.com>
2026-05-16 13:40:45 +0600

    updated

--- TaskManager/tasks/urls.py
+++ TaskManager/tasks/urls.py
@@ -4,6 +4,6 @@ from .views import TaskDetail, Tasklist
 app_name = 'tasks'
 
 urlpatterns = [
-    path('tasks/', Tasklist.as_view(), name='task-list'),
-    path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('tasks', Tasklist.as_view(), name='task-list'),
+    path('tasks/<int:pk>', TaskDetail.as_view(), name='task-detail'),
 ]
--- TaskManager/tasks/views.py
+++ TaskManager/tasks/views.py
@@ -7,6 +7,8 @@ from rest_framework import status
 from  rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import SAFE_METHODS, BasePermission, DjangoModelPermissions, IsAdminUser,DjangoModelPermissionsOrAnonReadOnly, IsAuthenticatedOrReadOnly
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 
@@ -18,28 +20,53 @@ class TaskUserWritePermission(BasePermission):
             return True
         return obj.user == request.user
 
+# Class based API view for handling tasks
+# class Tasklist(APIView):
+#     # authentication_classes = [TokenAuthentication]
+#     # permission_classes = [IsAdminUser]
+#     authentication_classes = [SessionAuthentication]
+#     queryset = Task.objects.all()
+#     # permission_classes = [DjangoModelPermissionsOrAnonReadOnly] # Allow read-only access for unauthenticated users, and full access for authenticated users with the appropriate model permissions.mane j j field e permission ase oi user er
+#     # permission_classes = [DjangoModelPermissions]
+#     permission_classes = [IsAuthenticated] # Allow read-only access for unauthenticated users, and full access for authenticated users.
+#     # permission_classes =[IsAuthenticatedOrReadOnly] 
 
-class Tasklist(APIView):
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAdminUser]
-    authentication_classes = [SessionAuthentication]
-    queryset = Task.objects.all()
-    # permission_classes = [DjangoModelPermissionsOrAnonReadOnly] # Allow read-only access for unauthenticated users, and full access for authenticated users with the appropriate model permissions.mane j j field e permission ase oi user er
-    # permission_classes = [DjangoModelPermissions]
-    permission_classes = [IsAuthenticated] # Allow read-only access for unauthenticated users, and full access for authenticated users.
-    # permission_classes =[IsAuthenticatedOrReadOnly]    
 
-    def get(self,request):
-        tasks= Task.objects.all()
-        serializer =TaskSerializer(tasks, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    def post(self,request):
-        serializer= TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def get(self,request):
+#         tasks= Task.objects.all()
+#         serializer =TaskSerializer(tasks, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     def post(self,request):
+#         serializer= TaskSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
+class CustomPaginationOne(PageNumberPagination):
+    page_size=3
+
+class CustomPaginationTwo(PageNumberPagination):
+
+    page_size_query_param='page_size'
+
+class CustomPaginationThree(PageNumberPagination):
+    page_size=2
+    page_size_query_param='page_size'           ## Allow clients to set the page size using a query parameter (e.g., ?page_size=5)
+    max_page_size=5    
+
+# Class based Generic APIview for handling tasks
+class Tasklist(ListCreateAPIView): 
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    # pagination_class = PageNumberPagination
+    pagination_class = CustomPaginationThree
+    
+
+
 class TaskDetail(APIView):
     # def get_object(self,pk):
     #     try:
